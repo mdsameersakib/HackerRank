c590e3f42690520f68c13d54a833a8e6f77e11a4
Md. Sameer Sakib <mdsameersakib@gmail.com>
2023-01-03 15:54:59 +0600

    Create String Validation.py

---
+++ String Validation.py
@@ -0,0 +1,16 @@
+'''Input Format
+A single line containing a string .
+
+Output Format
+In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+In the third line, print True if  has any digits. Otherwise, print False.
+In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if  has any uppercase characters. Otherwise, print False.'''
+
+user = input()
+    print(any(i.isalnum() for i in s))
+    print(any(i.isalpha() for i in s))
+    print(any(i.isdigit() for i in s))
+    print(any(i.islower() for i in s))
+    print(any(i.isupper() for i in s))
